3ef0f4558657697309d1aa670f255a40f738c14b
unknown <abelpaulo.durandpalacios21@gmail.com>
2025-11-12 01:08:35 -0500

    docs: comentando model para mejor entendimiento

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     protected $table = 'student';
-
+    /*Este seria como mi modelo , que debe coincidir con los datos de la tabla*/
     protected $fillable = [
         'name',
         'email',
